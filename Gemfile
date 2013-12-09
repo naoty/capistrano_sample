036ab1a4b647cc90281b62f252fde82faba050f2
Naoto Kaneko <naoty.k@gmail.com>
2013-12-09 18:02:44 +0900

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,31 @@
+source "https://rubygems.org"
+
+gem "rails", "4.0.2"
+gem "mysql2"
+gem "sass-rails", "~> 4.0.0"
+gem "uglifier", ">= 1.3.0"
+gem "coffee-rails", "~> 4.0.0"
+gem "jquery-rails"
+gem "turbolinks"
+gem "jbuilder", "~> 1.2"
+
+group :doc do
+  gem "sdoc", require: false
+end
+
+group :production do
+  gem "unicorn"
+end
+
+group :development, :test do
+  gem "spring"
+  gem "pry-rails"
+  gem "rspec-rails"
+end
+
+group :development do
+  gem "capistrano"
+  gem "capistrano-rails"
+  gem "capistrano-bundler"
+  gem "capistrano-rbenv"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CapistranoSample::Application.config.secret_key_base = '818490d68ec2e7a7149b7153a1e73200bb8e1396233a7017ecbdb929d9f63ae9473a051df1626f0ee3209ba7548366cf0f367e561b92017990b8635843897c87'
